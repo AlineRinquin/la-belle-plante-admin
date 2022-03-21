03b58b6cbc1628cab6f132f5ccac8c6d0bbecf04
Romain <51331158+zylfu@users.noreply.github.com>
2022-03-21 11:30:12 +0100

    mapping plantes ok

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Plant } from '../models/plant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  public collection$!: Observable<Plant[]>;
+  public subCollection$ = new Subject<Plant[]>();
+  private urlApi = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+    this.collection$ = this.http.get<any[]>(`${this.urlApi}/list_products`).pipe(
+      map(tabPlant => {
+        return tabPlant.map(obj => {
+          return new Plant(
+            obj.product_name,
+            obj.product_price,
+            obj.product_qty,
+            obj.product_instock,
+            obj.product_breadcrumb_label,
+            obj.product_url_page,
+            obj.product_id
+            );
+        })
+      })
+    );
+   }
+
 }
+
