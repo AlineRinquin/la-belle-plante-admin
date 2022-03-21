bcbf2a1c87a83e53849dfee1c27b72da63dc0884
Blandine Bajard <83599148+BlandineBajard@users.noreply.github.com>
2022-03-21 12:10:54 +0100

    un peu moisi mais ok

--- src/app/modules/admin/components/add-update-plant/add-update-plant.component.ts
+++ src/app/modules/admin/components/add-update-plant/add-update-plant.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-update-plant',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-update-plant.component.scss']
 })
 export class AddUpdatePlantComponent implements OnInit {
+  public ajoutPlantForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+  this.ajoutPlantForm = new FormGroup({});
+  }
 
   ngOnInit(): void {
+this.ajoutPlantForm = this.fb.group({
+  nomProduitFc: new FormControl(),
+  CategorieFc: new FormControl(),
+  prixFc: new FormControl(),
+  quantiteFc: new FormControl(),
+  stockFc: new FormControl(),
+})
   }
+
+    public onSubmit(): void {
+    }
+
 
 }
