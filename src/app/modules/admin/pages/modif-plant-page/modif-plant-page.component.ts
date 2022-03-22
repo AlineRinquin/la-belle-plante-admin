192d2848102402bfe18a9d95d7fb751afbff1d1f
AlineRinquin <aline.rinquin.ar@gmail.com>
2022-03-22 15:33:36 +0100

    modifications suite au passage avec jérémy

--- src/app/modules/admin/pages/modif-plant-page/modif-plant-page.component.ts
+++ src/app/modules/admin/pages/modif-plant-page/modif-plant-page.component.ts
@@ -10,12 +10,11 @@ import { AdminService } from '../../services/admin.service';
 })
 export class ModifPlantPageComponent implements OnInit {
   public data !: Plant;
-  public idPlant: string;
+  public idPlant!: string;
 
   constructor(private adminService: AdminService,
     private activatedRoute: ActivatedRoute,
     private route : Router) {
-    this.idPlant = '';
 
   }
 
@@ -24,13 +23,16 @@ export class ModifPlantPageComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(
       (resp: ParamMap) => {
         const plantId = resp.get('id')
-        this.adminService.getPlantById(plantId).subscribe(resp => {
-          console.log(resp);
-          this.data = resp;
-
+        if(plantId != null){
+          this.idPlant = plantId;
+          this.adminService.getPlantById(plantId).subscribe(resp => {
+            console.log(resp);
+            this.data = resp;
+          }
+          );
+        }else{
+          //page not found et message erreur
         }
-        );
-
       }
     )
 
@@ -39,17 +41,14 @@ export class ModifPlantPageComponent implements OnInit {
   }
 
   editPlant(form: Plant) {
-    this.activatedRoute.paramMap.subscribe(
-      (resp: ParamMap) => {
-        const plantId = resp.get('id')
-        this.adminService.update(form, plantId).subscribe(
-          resp =>{ console.log(resp)
-            
-          }
+    this.adminService.update(form, this.idPlant).subscribe(
+      resp => {
+        console.log(resp)
 
-        );
+      }
 
-      })
+    );
+
   }
 
 }
